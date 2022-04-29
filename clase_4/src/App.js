76a5ab50ce8afeca19d9b4f51872da5a218aa382
Mjhon03 <Mjhon1841@gmail.com>
2022-04-29 18:59:14 -0500

    Clase 4 teoria

---
+++ clase_4/src/App.js
@@ -0,0 +1,14 @@
+import MiComponente from './MiComponente';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h2>Reyes Godos: </h2>
+    <MiComponente rey={{nombre: "Atanagildo", reses: 12}}/>
+    <MiComponente rey={{nombre: "Ataulfo",reses: 18}}/>
+    </>
+  );
+}
+
+export default App;
---
+++ clase_4/src/MiComponente.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function MiComponente({rey}) {
+  return (
+    <div>
+        <h1>Hola {rey.nombre} come {rey.reses} vacas al dia</h1>
+    </div>
+  )
+}
+MiComponente.defaultProps = {
+    reses:"pocas",
+    rey:"Rey Godo",
+}
+export default MiComponente
