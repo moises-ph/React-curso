9c7279a2ea8bf69a65e42c9152435d9532bf4954
moisesph <mois.mp8@gmail.com>
2022-05-05 10:27:48 -0500

    calculadora terminada y actividad 6 terminada

--- actividad_chistes_video_6/src/App.js
+++ actividad_chistes_video_6/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
 
 function App() {
+  const [chiste, setChiste] = useState('');
+  const url = 'https://api.icndb.com/jokes/random';
+  const peticion = fetch(url);
+  const [repetir, setRepetir] = useState(0);
+
+  useEffect(() =>{
+    peticion.
+    then(datos => datos.json())
+    .then(lectura =>{
+      setChiste(
+      <div key={lectura.value.id}>{lectura.value.joke}</div>);
+      console.log(lectura.value.id);
+    });
+  }, [repetir]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <h1>Chiste</h1>
+    <div>{chiste}</div>
+    <button onClick={() => setRepetir(repetir+1)}>Otro chiste</button>
+    </>
+  )
 }
 
-export default App;
+export default App
